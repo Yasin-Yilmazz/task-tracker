4cf7fb36afbc323fda8668947e483c51782a4027
Yasin Yılmaz <yasinnyilmazz26@gmail.com>
2023-05-23 21:22:58 +0300

    add

--- todo/models.py
+++ todo/models.py
@@ -11,10 +11,10 @@ class Todo(models.Model):
     
     title = models.CharField(max_length=64)
     description = models.TextField(null=True, blank=True)
-    priority = models.PositiveSmallIntegerField(choice=PRIORITY, default=2)
+    priority = models.PositiveSmallIntegerField(choices=PRIORITY, default=2)
     is_done = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
