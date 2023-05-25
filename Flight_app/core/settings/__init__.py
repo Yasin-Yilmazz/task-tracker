b4daabb230f83272c7172c28026dce313c7ab1df
Yasin Yılmaz <yasinnyilmazz26@gmail.com>
2023-05-25 17:02:42 +0300

    static

--- Flight_app/core/settings/__init__.py
+++ Flight_app/core/settings/__init__.py
@@ -1,1 +1,9 @@
-from .development import *
+
+from decouple import config
+
+ENVRIONMENT = config('ENV')
+
+if ENVRIONMENT=='development':
+    from .development import *
+else:
+    from .production import *
